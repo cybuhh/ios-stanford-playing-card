9ff5492750208636e060121ac45fb513a5de9ea0
cybuhh <>
2017-12-17 10:59:56 +0100

    drawing rouded corners

--- playing-card/PlayingCardView.swift
+++ playing-card/PlayingCardView.swift
@@ -10,20 +10,9 @@ import UIKit
 
 class PlayingCardView: UIView {
     override func draw(_ rect: CGRect) {
-        /*if let context = UIGraphicsGetCurrentContext() {
-            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-            context.setLineWidth(5.0)
-            UIColor.green.setFill()
-            UIColor.red.setStroke()
-            context.strokePath()
-            context.fillPath()
-        }*/
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.lineWidth = 5.0
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.stroke()
-        path.fill()
+        let roudedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roudedRect.addClip()
+        UIColor.white.setFill()
+        roudedRect.fill()
     }
 }
